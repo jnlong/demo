7839fbdaa36d59982ad83fb2749af2fd01223da5
zhangxianlong <zhangxianlong@baidu.com>
2016-08-10 18:16:29 +0800

    update

--- reptile-demo.js
+++ reptile-demo.js
@@ -2,4 +2,8 @@ var reptile = require('./lib/reptile-api.js');
 var log4js = require('log4js');
 log4js.configure('./config/log4js.json');
 
-reptile({ curPath: 'http://nodejs.cn/doc/node', fileName: 'index.html', menuFilter: '#column2 ul a', path: __dirname + '\\out\\' });
+// 爬取nodejs官网api
+// reptile({ curPath: 'http://nodejs.cn/doc/node', fileName: 'index.html', menuFilter: '#column2 ul a', path: __dirname + '\\out\\' });
+
+// 爬取mdn 官网api
+reptile({curPath: 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript', menuFilter: '#quick-links ol a', path: __dirname + '\\mdn-js\\' })
