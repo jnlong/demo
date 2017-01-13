f3480f21e3170117db5c3fe05bcc04fef32a0a8b
jnlong <zxl8310@163.com>
2017-01-13 19:12:55 +0800

    add node-cms

---
+++ node/readDataToCms_tran.js
@@ -0,0 +1,66 @@
+/*
+ * @author zxl
+ * @date 2017-01-13
+ * @file 从txt文件中读取数据，转换成数组格式，然后向cms中追加数据
+ */
+var fs = require('fs');
+var path = require('path');
+var encoding = require("encoding");
+var filePath = 'D:\\mobile\\';
+var resArr = [];
+var obj;
+var list = {
+    title: '应用名称',
+    app_file_size: 'apk大小',
+    download_url: 'apk下载地址',
+    des: '描述',
+    type: '应用类型',
+    icon: 'icon地址',
+    images: '截图地址',
+    // 没有数据的key
+    category: '',
+    color: '',
+    description: '应用简介',
+    downnum: '下载数量',
+    more: '更多',
+    num: '',
+    pos: '',
+    style: '',
+    url: '',
+};
+
+init();
+
+function init() {
+    var files = fs.readdirSync(filePath);
+    for (var i in files) {
+        var file = path.join(filePath, files[i]);
+        var data = fs.readFileSync(file);
+        log(data);
+        var newData = encoding.convert(data, 'UTF-8', 'GBK');
+        fs.writeFile(path.join(filePath + 'new\\', files[i]), newData);
+    }
+    log(resArr);
+    // fs.writeFile('new.js', JSON.stringify(resArr));
+}
+
+function log(m) {
+    console.log(m);
+}
+
+function getJson(data) {
+    var arr = data.replace(/(\r|\n)/g, '').split('@');
+    obj = {};
+    for (var i = 0; i < arr.length; i++) {
+        var cur = arr[i];
+        for (j in list) {
+            if (!list[j]) {
+                obj[j] = listDef[j] || '';
+            } else if (cur.indexOf(list[j]) != -1) {
+                obj[j] = cur.replace(list[j] + '|', '');
+            }
+        }
+    }
+    log(obj)
+    resArr.push(obj);
+}
